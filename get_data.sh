c7bb0eecf2f1a736293eddbd94f7f6fb75520239
YovaKem <yova.k1@gmail.com>
2023-06-15 13:00:32 +0200

    download both versions of immutable data

--- get_data.sh
+++ get_data.sh
@@ -11,6 +11,8 @@ wget https://huggingface.co/datasets/Yova/templama/resolve/main/train_with_alias
 wget https://huggingface.co/datasets/Yova/templama/resolve/main/val_with_aliases.json
 wget https://huggingface.co/datasets/Yova/templama/resolve/main/test_with_aliases.json
 
+# immutable
+wget https://huggingface.co/datasets/Yova/immutable_facts/resolve/main/immutable.json
 wget https://huggingface.co/datasets/Yova/immutable_facts/resolve/main/immutable_with_aliases.json
 
 cd ..
