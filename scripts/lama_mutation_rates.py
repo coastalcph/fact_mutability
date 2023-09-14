5414cabbfa062b1d6ad3db58b896e31d8c36c560
Nicolas Garneau <nicolas@nlp.quebec>
2023-09-14 10:42:14 +0200

    Remove parallelism

--- scripts/lama_mutation_rates.py
+++ scripts/lama_mutation_rates.py
@@ -12,7 +12,6 @@ def query_wikidata(query):
     client = Client()
     examples = list()
     subj_id, relation = query.id.split("_") 
-    print(subj_id, relation)
     subj = client.get(subj_id, load=True)
     if relation in subj.attributes['claims']:
         for instance in subj.attributes['claims'][relation]:
@@ -65,62 +64,11 @@ def query_wikidata(query):
 
 def main():
     data = list()
-    client = Client()
     immutable_dataset = build_dataset('data/lama.json')  # LAMA
     print(len(immutable_dataset))
-    with Pool(24) as p:
-        res = tqdm(p.map(query_wikidata, immutable_dataset), total=len(immutable_dataset))
-    data = list(map(sum, res))
-
-    # for query in tqdm(immutable_dataset):
-    #     subj_id, relation = query.id.split("_") 
-    #     print(subj_id, relation)
-    #     subj = client.get(subj_id, load=True)
-    #     if relation in subj.attributes['claims']:
-    #         for instance in subj.attributes['claims'][relation]:
-    #             obj_id = instance['mainsnak']['datavalue']['value']['id']
-    #             obj = client.get(obj_id, load=True)
-    #             if 'qualifiers' in instance:
-    #                 if start_time in instance['qualifiers'] and end_time in instance['qualifiers']:
-    #                     start = instance['qualifiers'][start_time][0]['datavalue']['value']['time']
-    #                     end = instance['qualifiers'][end_time][0]['datavalue']['value']['time']
-    #                     example = {
-    #                         "subj_id": subj_id,
-    #                         "subj": subj.label,
-    #                         "relation": relation,
-    #                         "obj_id": obj_id,
-    #                         "obj": obj.label,
-    #                         "start_time": start,
-    #                         "end_time": end
-    #                     }
-    #                     data.append(example)
-    #                 else:
-    #                     example = {
-    #                         "subj_id": subj_id,
-    #                         "subj": subj.label,
-    #                         "relation": relation,
-    #                         "obj_id": obj_id,
-    #                         "obj": obj.label,
-    #                     }
-    #                     print("No time qualifiers, no mutability")
-    #             else:
-    #                 example = {
-    #                     "subj_id": subj_id,
-    #                     "subj": subj.label,
-    #                     "relation": relation,
-    #                     "obj_id": obj_id,
-    #                     "obj": obj.label,
-    #                 }
-    #                 print("No qualifiers")
-    #     else:
-    #         example = {
-    #             "subj_id": subj_id,
-    #             "subj": subj.label,
-    #             "relation": relation
-    #         }
-    #         print(f"No claim of {subj.label} for rel {relation}")
-    #     data.append(example)
-
+    for query in tqdm(immutable_dataset, total=len(immutable_dataset)):
+        exs = query_wikidata(query)
+        data += exs
     json.dump(data, open("./data/lama_with_dates.json", "w"))
 
 if __name__ == '__main__':
