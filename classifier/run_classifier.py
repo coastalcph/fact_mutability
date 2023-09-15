8d8a0c18ad823e84df913087dfd33fa9984cd037
cfierro94 <constanza.fierro94@gmail.com>
2023-09-15 11:36:48 +0200

    Add t5 support for probing classifier

--- classifier/run_classifier.py
+++ classifier/run_classifier.py
@@ -30,19 +30,16 @@ import wandb
 from datasets import load_dataset
 from transformers import (
     EarlyStoppingCallback,
-    LlamaModel,
-    LlamaPreTrainedModel,
     AutoTokenizer,
     DataCollatorWithPadding,
     HfArgumentParser,
     Trainer,
     TrainingArguments,
 )
-from transformers.modeling_outputs import SequenceClassifierOutputWithPast
-from typing import Tuple, Union
-from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
-from torch import nn
-
+from models import (
+    LlamaForSequenceClassificationPerLayer,
+    T5ForSequenceClassificationPerLayer,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -88,132 +85,6 @@ class ModelArguments:
         }
     )
     train_classifier_from_layer: int = field(default=-1)
-
-
-class LlamaForSequenceClassificationPerLayer(LlamaPreTrainedModel):
-    """This is a copy only changing the hidden_state being used.
-    The original code was copied from transformers.models.llama.modeling_llama.LlamaForSequenceClassification
-    """
-
-    def __init__(self, config, hidden_states_from_layer=-1):
-        super().__init__(config)
-        self.num_labels = config.num_labels
-        self.model = LlamaModel(config)
-        self.model.eval()
-        self.score = nn.Linear(config.hidden_size, self.num_labels, bias=False)
-        self.hidden_states_from_layer = hidden_states_from_layer
-
-        # Initialize weights and apply final processing
-        self.post_init()
-
-    def get_input_embeddings(self):
-        return self.model.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.model.embed_tokens = value
-
-    def forward(
-        self,
-        input_ids: torch.LongTensor = None,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[List[torch.FloatTensor]] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, SequenceClassifierOutputWithPast]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
-            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
-            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
-            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-        """
-        return_dict = (
-            return_dict if return_dict is not None else self.config.use_return_dict
-        )
-        with torch.no_grad():
-            transformer_outputs = self.model(
-                input_ids,
-                attention_mask=attention_mask,
-                position_ids=position_ids,
-                past_key_values=past_key_values,
-                inputs_embeds=inputs_embeds,
-                use_cache=use_cache,
-                output_attentions=output_attentions,
-                output_hidden_states=True,
-                return_dict=return_dict,
-            )
-        # hidden_states = transformer_outputs[0]
-        hidden_states = transformer_outputs.hidden_states[self.hidden_states_from_layer]
-        transformer_outputs.hidden_states = (
-            None  # TODO: check this is what we want, trying for OOM
-        )
-        logits = self.score(hidden_states)
-
-        if input_ids is not None:
-            batch_size = input_ids.shape[0]
-        else:
-            batch_size = inputs_embeds.shape[0]
-
-        if self.config.pad_token_id is None and batch_size != 1:
-            raise ValueError(
-                "Cannot handle batch sizes > 1 if no padding token is defined."
-            )
-        if self.config.pad_token_id is None:
-            sequence_lengths = -1
-        else:
-            if input_ids is not None:
-                sequence_lengths = (
-                    torch.eq(input_ids, self.config.pad_token_id).long().argmax(-1) - 1
-                ).to(logits.device)
-            else:
-                sequence_lengths = -1
-
-        pooled_logits = logits[
-            torch.arange(batch_size, device=logits.device), sequence_lengths
-        ]
-
-        loss = None
-        if labels is not None:
-            labels = labels.to(logits.device)
-            if self.config.problem_type is None:
-                if self.num_labels == 1:
-                    self.config.problem_type = "regression"
-                elif self.num_labels > 1 and (
-                    labels.dtype == torch.long or labels.dtype == torch.int
-                ):
-                    self.config.problem_type = "single_label_classification"
-                else:
-                    self.config.problem_type = "multi_label_classification"
-
-            if self.config.problem_type == "regression":
-                loss_fct = MSELoss()
-                if self.num_labels == 1:
-                    loss = loss_fct(pooled_logits.squeeze(), labels.squeeze())
-                else:
-                    loss = loss_fct(pooled_logits, labels)
-            elif self.config.problem_type == "single_label_classification":
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(
-                    pooled_logits.view(-1, self.num_labels), labels.view(-1)
-                )
-            elif self.config.problem_type == "multi_label_classification":
-                loss_fct = BCEWithLogitsLoss()
-                loss = loss_fct(pooled_logits, labels)
-        if not return_dict:
-            output = (pooled_logits,) + transformer_outputs[1:]
-            return ((loss,) + output) if loss is not None else output
-
-        return SequenceClassifierOutputWithPast(
-            loss=loss,
-            logits=pooled_logits,
-            past_key_values=transformer_outputs.past_key_values,
-            hidden_states=transformer_outputs.hidden_states,
-            attentions=transformer_outputs.attentions,
-        )
 
 
 def replace_subject(tokenizer, example):
@@ -296,13 +167,24 @@ def main(device):
     id2label = {1: "MUTABLE", 0: "IMMUTABLE"}
     label2id = {"MUTABLE": 1, "IMMUTABLE": 0}
 
-    model = LlamaForSequenceClassificationPerLayer.from_pretrained(
-        model_args.model_name_or_path,
-        num_labels=2,
-        id2label=id2label,
-        label2id=label2id,
-        hidden_states_from_layer=model_args.train_classifier_from_layer,
-    ).to(device)
+    if "t5" in model_args.model_name_or_path:
+        model = T5ForSequenceClassificationPerLayer.from_pretrained(
+            model_args.model_name_or_path,
+            load_in_8bit=True,
+            device_map="auto",
+            num_labels=2,
+            id2label=id2label,
+            label2id=label2id,
+            hidden_states_from_layer=model_args.train_classifier_from_layer,
+        ).to(device)
+    else:
+        model = LlamaForSequenceClassificationPerLayer.from_pretrained(
+            model_args.model_name_or_path,
+            num_labels=2,
+            id2label=id2label,
+            label2id=label2id,
+            hidden_states_from_layer=model_args.train_classifier_from_layer,
+        ).to(device)
 
     trainer = Trainer(
         model=model,
