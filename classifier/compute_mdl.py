e097f54ff03543eea1e25d0bc89f921873847c0c
cfierro94 <constanza.fierro94@gmail.com>
2023-10-18 14:42:31 +0200

    Add model code_length computation

--- classifier/compute_mdl.py
+++ classifier/compute_mdl.py
@@ -5,33 +5,53 @@ from glob import glob
 import numpy as np
 from datasets import load_dataset
 
+RESULTS_DIR = "/projects/nlp/data/constanzam/mdl_mutability/"
+
+
+def get_acc(dir_name, n=10):
+    f = glob(os.path.join(dir_name, f"{n}_*/test_results.json"))
+    assert len(f) == 1
+    with open(f[0]) as test_results:
+        return json.load(test_results)["eval_accuracy"]
+
+
+def get_code_length(dir_name, n):
+    f = glob(os.path.join(dir_name, f"{n}_*/online_portion_results.json"))
+    assert len(f) == 1
+    with open(f[0]) as eval_results:
+        return json.load(eval_results)["eval_sum_log2_prob"]
+
+
 def compute_online_codelength(dir_name):
     log2 = collections.defaultdict(dict)
-    for f in glob(os.path.join(dir_name, '**/eval_results.json')):
-        if f.startswith(os.path.join(dir_name, 'fixed/10')):
-            continue
-        t = f[len(dir_name)]
-        with open(f) as eval_results:
-            data = json.load(eval_results)
-            log2[t] = data['eval_sum_log2_prob']
-    return log2['0'] - sum([log2[str(i)] for i in range(1, 10)])
+    for i in range(0, 10):
+        log2[i] = get_code_length(dir_name, i)
+    print(dir_name)
+    print(log2)
+    return log2[0] - sum([log2[i] for i in range(1, 10)])
+
 
 def compute_compression(codelength, ds):
-    portion_sizes = [0.1, 0.2, 0.4, 0.8, 1.6, 3.2, 6.25, 12.5, 25, 50, 100]
-    portion_indices = [
-        int(portion_size * 0.01 * len(ds["train"]))
-        for portion_size in portion_sizes
-    ]
     num_classes = 2
     uniform_codelength = len(ds["train"]) * np.log2(num_classes)
     return uniform_codelength / codelength
 
-ds = load_dataset("cfierro/mutability_classifier_data", use_auth_token=True)
 
-online_codelength = compute_online_codelength('/projects/nlp/data/constanzam/mdl_mutability/normal/')
-compression = compute_compression(online_codelength, ds)
-print(f'normal labels: codelength={online_codelength} compression={compression}')
+def print_metrics(dir_name):
+    online_codelength = compute_online_codelength(dir_name)
+    compression = compute_compression(online_codelength, ds)
+    cl_trained_all_data = get_code_length(dir_name, 10)
+    # model_cl + correction = online_cl
+    # model_cl = online_cl - correction
+    print(
+        "normal labels: codelength={} compression={} model_cl={}".format(
+            online_codelength, compression, online_codelength - cl_trained_all_data
+        )
+    )
+    print("Final prob. acc:", get_acc(dir_name))
 
-online_codelength = compute_online_codelength('/projects/nlp/data/constanzam/mdl_mutability/random_r/')
-compression = compute_compression(online_codelength, ds)
-print(f'random labels: codelength={online_codelength} compression={compression}')
+
+ds = load_dataset("coastalcph/fm_queries_classifier", use_auth_token=True)
+print_metrics(os.path.join(RESULTS_DIR, "normal_fm_dataset/"))
+print()
+print_metrics(os.path.join(RESULTS_DIR, "random_fm_dataset/"))
