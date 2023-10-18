d810de57527a49a7490ce34c448db1a3158b87eb
cfierro94 <constanza.fierro94@gmail.com>
2023-10-18 14:43:00 +0200

    Make classifier dataset to only use 1 of 5 templates

--- classifier/prepare_classifier_data.py
+++ classifier/prepare_classifier_data.py
@@ -1,7 +1,8 @@
 from datasets import load_dataset
 from collections import Counter
+import numpy as np
 
-split_to_relation = {
+SPLIT_TO_RELATIONS = {
     "train": [
         "P103",
         "P19",
@@ -22,13 +23,22 @@ split_to_relation = {
 
 if __name__ == "__main__":
     ds = load_dataset("coastalcph/fm_queries")
-    ds["all"] = ds["train"]
-    print("Examples per relation", Counter(ds["all"]["relation"]))
-    for split in split_to_relation.keys():
-        ds[split] = ds["all"].filter(
-            lambda ex: ex["relation"] in split_to_relation[split]
+    ds["all_fm"] = ds["train"]
+    rng = np.random.default_rng(7)
+    print("Examples per relation", Counter(ds["all_fm"]["relation"]))
+    for split in SPLIT_TO_RELATIONS.keys():
+        ds[split] = ds["all_fm"].filter(
+            lambda ex: ex["relation"] in SPLIT_TO_RELATIONS[split]
         )
-        assert set(ds[split]["type"]).intersection(["immutable_n"]) == set()
+        tuple_id_to_index = {
+            s: i for i, s in enumerate(list(set([id_[:-2] for id_ in ds[split]["id"]])))
+        }
+        assert int(len(ds[split]) / 5) == len(tuple_id_to_index)
+        template_choice = rng.choice(5, int(len(ds[split]) / 5))
+        ds[split] = ds[split].filter(
+            lambda ex: template_choice[tuple_id_to_index[ex["id"][:-2]]]
+            == int(ex["id"][-1])
+        )
     ds = ds.map(lambda ex: {"is_mutable": 1 if ex["type"] == "mutable" else 0})
     print(ds)
     print("Pushing to the hub...")
