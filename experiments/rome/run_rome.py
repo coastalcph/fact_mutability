8c390a8fbb6e6298701e2724f4d3008b8f974bca
cfierro94 <constanza.fierro94@gmail.com>
2023-11-15 09:44:42 +0100

    fix old answer used

--- experiments/rome/run_rome.py
+++ experiments/rome/run_rome.py
@@ -75,7 +75,7 @@ def main(args):
         for ex in ds:
             subj = ex["query"]["label"]
             prompt = ex["prediction"]["query"].replace(subj, "{}")
-            old_answer = ex["prediction"]["predictions"][0]["answer"]
+            old_answer = ex["original_answer"]
             for update in ex["updates"]:
                 requests.append(
                     {
