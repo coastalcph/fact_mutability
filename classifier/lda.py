608f1bd4e0f08d5b3ad5e0fd13190b4a5f0be442
cfierro94 <constanza.fierro94@gmail.com>
2023-11-29 19:09:29 +0100

    Add option to run lda for every layer and cached computations

--- classifier/lda.py
+++ classifier/lda.py
@@ -13,14 +13,15 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
 )
-
+import numpy as np
+import collections
 from classifier.mdl_classifier import INSTRUCTION, TEMPLATE_TO_USE, replace_subject
 from inference import prepare_prompt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def main(args):
+def get_hidden_states_repr(args):
     dataset_name = "coastalcph/mutability_classifier-1-{}"
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     tokenizer.pad_token = tokenizer.eos_token
@@ -42,49 +43,86 @@ def main(args):
     model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).to(device)
 
     y = []
-    X = []
+    X = None
     relations = []
     mut_types = []
     type_to_label = {
         t: i for i, t in enumerate(["immutable", "immutable_n", "mutable"])
     }
-    with torch.no_grad():
-        for tokenized_ds in tokenized_datasets:
-            for ex in tqdm(tokenized_ds[args.split]):
-                y.append(type_to_label[ex["type"]])
-                relations.append(ex["relation"])
-                mut_types.append(ex["type"])
+    for tokenized_ds in tokenized_datasets:
+        for ex in tqdm(tokenized_ds[args.split]):
+            y.append(type_to_label[ex["type"]])
+            relations.append(ex["relation"])
+            mut_types.append(ex["type"])
+            with torch.no_grad():
                 outputs = model(
                     input_ids=torch.tensor(ex["input_ids"]).to(device),
                     attention_mask=torch.tensor(ex["attention_mask"]).to(device),
                     output_hidden_states=True,
                 )
-                X.append(outputs.hidden_states[args.layer][0, -1, :].cpu().numpy())
-    print("Fitting LDA...")
-    clf = LinearDiscriminantAnalysis()
-    clf.fit(X, y)
-    print("explained_variance_ratio_", clf.explained_variance_ratio_)
-    wandb.run.summary["explained_variance_ratio_"] = clf.explained_variance_ratio_
-    X_transformed = clf.transform(X)
+            layers = (
+                [args.layer]
+                if args.layer is not None
+                else range(len(outputs.hidden_states))
+            )
+            if X is None:
+                X = [[] for _ in layers]
+            for layer in layers:
+                X[layer].append(outputs.hidden_states[layer][0, -1, :].cpu().numpy())
+    return X, y, relations, mut_types
 
-    df = pd.DataFrame(
-        zip(X_transformed[:, 0], X_transformed[:, 1], mut_types, relations),
-        columns=["1st. dim", "2nd. dim", "mut_type", "relation"],
-    )
-    fig = px.scatter(
-        df,
-        x="1st. dim",
-        y="2nd. dim",
-        color="relation",
-        symbol="mut_type",
-        symbol_sequence=[0, "diamond-open", 304],
-        title=f"LDA of classifier {args.split} data (reprs@layer={args.layer})",
-    )
-    out_image_file = os.path.join(
-        args.output_folder, f"{args.model_name}_{args.split}.pdf"
-    )
-    fig.write_image(out_image_file)
-    wandb.config["out_image_file"] = out_image_file
+
+def main(args):
+    output_folder = os.path.join(args.output_folder, args.model_name, args.split)
+    wandb.config["final_output_folder"] = output_folder
+    cache_filename = os.path.join(output_folder, "hidden_states_per_layer.npz")
+    if os.path.exists(cache_filename):
+        print("Loading data from cache file", cache_filename)
+        numpy_result = dict(np.load(cache_filename, allow_pickle=True))
+        X, y, relations, mut_types = (
+            numpy_result["X"],
+            numpy_result["y"],
+            numpy_result["relations"],
+            numpy_result["mut_types"],
+        )
+    else:
+        X, y, relations, mut_types = get_hidden_states_repr(args)
+        np.savez(
+            cache_filename,
+            **{"X": X, "y": y, "relations": relations, "mut_types": mut_types},
+        )
+    for layer in range(len(X)):
+        cache_filename = os.path.join(output_folder, f"X_transformed_{layer}.npz")
+        if os.path.exists(cache_filename):
+            print("Loading cached X_transformed from", cache_filename)
+            numpy_result = dict(np.load(cache_filename, allow_pickle=True))[
+                "X_transformed"
+            ]
+        else:
+            print("Fitting LDA...", layer)
+            clf = LinearDiscriminantAnalysis()
+            clf.fit(X, y)
+            print("explained_variance_ratio_", clf.explained_variance_ratio_)
+            wandb.run.summary[
+                f"explained_variance_ratio_{layer}"
+            ] = clf.explained_variance_ratio_
+            X_transformed = clf.transform(X)
+
+        df = pd.DataFrame(
+            zip(X_transformed[:, 0], X_transformed[:, 1], mut_types, relations),
+            columns=["1st. dim", "2nd. dim", "mut_type", "relation"],
+        )
+        fig = px.scatter(
+            df,
+            x="1st. dim",
+            y="2nd. dim",
+            color="relation",
+            symbol="mut_type",
+            symbol_sequence=[0, "diamond-open", 304],
+            title=f"LDA of classifier {args.split} data (reprs@layer={layer})",
+        )
+        out_image_file = os.path.join(output_folder, f"lda_layer{layer}.pdf")
+        fig.write_image(out_image_file)
 
 
 if __name__ == "__main__":
@@ -115,7 +153,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--layer",
-        default=-1,
+        default=None,
         type=int,
         help="",
     )
